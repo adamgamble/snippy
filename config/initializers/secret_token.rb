f4b54adca2477babce3a9aea5b6a5aaca01a0a62
Adam Gamble <adamgamble@Adam-Gambles-MacBook-Pro.local>
2011-07-27 00:01:31 -0500

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snippy::Application.config.secret_token = '5fc5943dd77b1be84fe8cd2a5780026d6353255b40edd3461bebcff79decc207e756331b18f7e36580301d65b29cf0fa9472c387d1dace43d7f2811ea3ac4cc8'
